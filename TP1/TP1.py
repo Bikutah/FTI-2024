abe1c2c0a3459d6dce2c8711133e83c8536ac694
victor hugo cretton rivera <victorcrettonrivera@gmail.com>
2024-09-29 16:10:43 -0300

    Archivo de llegada y archivo de respuesta y comentarios extras

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -1,27 +1,38 @@
 import re
 
-
 def main():
-    print("Bienvenido!")
 
-    # Abrimos los archivos
-    archivo_llegada = open("archivo_llegada.txt", "r")
-    archivo_respuesta = open("archivo_respuesta.txt", "w")
+        ruta_llegada = input('Ingrese la ruta del archivo (ejemplo: archivo_leer)\n')
+        archivo_llegada = open(f'{ruta_llegada}.txt', "r")
 
-    # Solicitamos el patrón al usuario
-    patrones = input('Ingrese el patrón separado por comas (ejemplo: man,s,tu)\n')
+        ruta_respuesta = input('Ingrese el nombre del archivo donde se guardara (ejemplo: archivo_respuesta)\n')
+        archivo_respuesta = open(f'{ruta_respuesta}.txt', "w")
 
-    # Separamos el input en una lista de patrones
-    lista_patrones = patrones.split(',')
+        # Solicitamos el patrón al usuario
+        patrones = input('Ingrese el patrón separado por comas (ejemplo: man,s,tu)\n')
 
-    patron = '|'.join([fr'\b\w*{patron}\w*\b' for patron in lista_patrones])
+        # Separamos el input en una lista de patrones
+        lista_patrones = patrones.split(',')
 
-    print(f'Patrón generado: {patron}')  # Esto imprime el patrón generado para verificar
+        # Esta funcion genera nuestra expresion regular
+        # {a..z+A..Z+1..0}*.(Patron).{a..z+A..Z+1..0} | {a..z+A..Z+1..0}*.(Patron).{a..z+A..Z+1..0}
+        patron = '|'.join([fr'\w*{patron}\w*' for patron in lista_patrones])
+        print(f'Patrón generado: {patron}')
 
-    archivo_respuesta.write(re.sub(patron, '', archivo_llegada.read()))
+        # Se reemplaza el patron por ''
+        archivo_respuesta.write(re.sub(patron, '', archivo_llegada.read()))
 
-    archivo_llegada.close()
-    archivo_respuesta.close()
+        print("Su archivo se genero con exito en la carpeta del proyecto :)")
+
+        # Se cierran los archivos
+        archivo_llegada.close()
+        archivo_respuesta.close()
 
 if __name__ == '__main__':
-    main()
+    try:
+        print("╔" + "═" * 13 + "╗")
+        print("║ Bienvenido! ║")
+        print("╚" + "═" * 13 + "╝")
+        main()
+    except:
+        print("Ocurrio un error inesperado :(")
